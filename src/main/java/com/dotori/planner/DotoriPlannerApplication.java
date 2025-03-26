be4f808210b32f5636eca8e95d309b545fe21885
minddol <aladin1994@naver.com>
2025-03-26 14:48:08 +0900

    도토리 플래너 초기 커밋

---
+++ src/main/java/com/dotori/planner/DotoriPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.dotori.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotoriPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotoriPlannerApplication.class, args);
+	}
+
+}
